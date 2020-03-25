7749ae55b9649482810bc664bb5eb7fc0b08d73e
Mônica Colomera <colomeramonica@gmail.com>
2020-03-25 16:53:33 -0300

    alteração na lógica. aplicação parcialmente funcional

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,98 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Olá, mundo!"/>
+    <h1><i>PicPager</i></h1>
+    <hr />
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <input type="file"  v-on:submit="addFiles" name="post__img" class="float-left mb-3">
+        </div>
+        <div class="col-lg-12">
+          <textarea placeholder="Descrição" name="post__desc"
+          class="form-control" v-model="post_desc"></textarea>
+          <button v-on:click.prevent="addPost" type="submit" class="mt-3 float-left btn btn-primary">Enviar</button>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 list-group">
+            <h4 class="mt-3 text-left">Comentários</h4>
+            <div class="form-todo form-group">
+            <p>
+              <input type="text" class="form-control" name="comment__author" placeholder="Autor" v-model="comment_author"/>
+            </p>
+            <p>
+              <textarea placeholder="Mensagem" name="comment__msg"
+              class="form-control" v-model="comment_msg" ></textarea>
+            </p>
+            <button type="submit" v-on:click="addComment" class="float-left btn btn-primary">Novo Comentário</button>
+          </div>
+        </div>
+      </div>
+        <div class="list-group-item text-left" v-for="(comment, idx) in comments" v-bind:key="(comment, idx)">
+          <span class="comment__author">Autor: <strong>{{ comment.author }}</strong></span>
+            <div></div>
+          <span class="comment__message">Mensagem: <strong>{{ comment.message }}</strong></span>
+          <div>
+            <a href="#" v-on:click.prevent="removeComment(idx)" title="Excluir">Excluir</a>
+          </div>
+        </div>
+      </div>
+      <hr />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      comments: [],
+      posts: [],
+      post_img: '',
+      post_desc: '',
+      comment_author: '',
+      comment_msg: '' 
+    }
+  },
+  methods: {
+    addPost() {
+      let file = this.addFiles;
+      this.posts.push({
+        img: e.target.files[0],
+        description: this.post_desc
+      });
+
+      this.post_desc = '';
+      console.log(this.posts);
+    },
+    addComment() {
+      this.comments.push({
+        author: this.comment_author,
+        message: this.comment_msg
+      });
+
+      this.comment_author = '';
+      this.comment_msg = '';
+    },
+    addFiles(e) {
+      var file;
+      if (typeof(e) != 'undefined')
+        file = e.target.files[0];
+      
+    },
+    removePost(id) {
+      this.posts.splice(id, 1);
+    },
+    removeComment(id) {
+      this.comments.splice(id, 1);
+    }
   }
 }
 </script>
 
 <style>
-#app {
+#app {  
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueResource from 'vue-resource';
+import { createPopper } from '@popperjs/core';
+
+Vue.use(VueResource);
+Vue.use(createPopper);
 Vue.config.productionTip = false
 
 new Vue({
